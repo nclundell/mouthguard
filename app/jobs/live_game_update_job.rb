122d518e032afe758f0dd5faee48ce175482a81a
Nathan Lundell <nclundell@pm.me>
2024-12-22 14:35:50 -0500

    move live update to a job

---
+++ app/jobs/live_game_update_job.rb
@@ -0,0 +1,50 @@
+require 'net/http'
+
+class LiveGameUpdateJob < ApplicationJob
+  queue_as :live_update
+
+  def perform(*args)
+    puts "Updating live games..."
+
+    season = Rails.application.credentials.season
+
+    uri = URI("https://apinext.collegefootballdata.com/scoreboard?classification=fbs")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request["Authorization"] = "Bearer #{Rails.application.credentials.cfbd_key}"
+    request["Content-Type"] = "application/json"
+
+    response = http.start do |http|
+      http.request(request)
+    end
+
+    games = JSON.parse(response.body)
+
+    games.each do |game_data|
+      game = Game.find_or_initialize_by(cfbd_id: game_data["id"])
+      if game.is_live?
+        game["period"]     = game_data["period"]
+        game["clock"]      = game_data["clock"]
+        game["situation"]  = game_data["situation"]
+        game["possession"] = game_data["possession"]
+        game["last_play"]  = game_data["lastPlay"]
+      else
+        game["period"]     = nil
+        game["clock"]      = nil
+        game["situation"]  = nil
+        game["possession"] = nil
+        game["last_play"]  = nil
+      end
+      game["tv"]               = game_data["tv"]
+      game["completed"]        = game_data["status"] == "completed" ? true : false
+      game["home_points"]      = game_data["homeTeam"]["points"]
+      game["home_line_scores"] = game_data["homeTeam"]["lineScores"]
+      game["away_points"]      = game_data["awayTeam"]["points"]
+      game["away_line_scores"] = game_data["awayTeam"]["lineScores"]
+      game.save!
+    end
+  end
+end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,9 @@ class Game < ApplicationRecord
   belongs_to :venue
   belongs_to :home, class_name: "Team"
   belongs_to :away, class_name: "Team"
+
+  def is_live?
+    return false if completed
+    Time.now > start
+  end
 end
