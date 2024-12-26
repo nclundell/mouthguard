55676073a5675ed2c1e461c1fcd1983d17120ae2
Nathan Lundell <nclundell@pm.me>
2024-12-25 23:46:41 -0500

    user name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
   has_many :picks
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  def name
+    return display_name unless display_name.blank?
+    return first_name unless first_name.blank?
+
+    email_address
+  end
 end
