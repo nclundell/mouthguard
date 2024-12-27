3140960bf511062973df83e15b4fea26fdb29b2c
Nathan Lundell <nclundell@pm.me>
2024-12-27 13:48:26 -0500

    allow for games that go past midnight to still show on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   allow_unauthenticated_access
 
   def index
-    @games = Game.all.select { |g| g.today? }
+    @games = Game.all.select { |g| g.today? || g.is_live? }
   end
 end
