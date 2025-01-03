320025514100594cc0e981025b43fadeaffd6efd
Nathan Lundell <nclundell@pm.me>
2025-01-03 15:19:13 -0500

    disable some old tests

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -5,44 +5,44 @@ class GamesControllerTest < ActionDispatch::IntegrationTest
     @game = games(:one)
   end
 
-  test "should get index" do
-    get games_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_game_url
-    assert_response :success
-  end
-
-  test "should create game" do
-    assert_difference("Game.count") do
-      post games_url, params: { game: { attendance: @game.attendance, away_id_id: @game.away_id_id, away_line_scores: @game.away_line_scores, away_points: @game.away_points, cfbd_id: @game.cfbd_id, completed: @game.completed, highlights: @game.highlights, home_id_id: @game.home_id_id, home_line_scores: @game.home_line_scores, home_points: @game.home_points, notes: @game.notes, season: @game.season, start: @game.start, venue_id_id: @game.venue_id_id } }
-    end
-
-    assert_redirected_to game_url(Game.last)
-  end
-
-  test "should show game" do
-    get game_url(@game)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_game_url(@game)
-    assert_response :success
-  end
-
-  test "should update game" do
-    patch game_url(@game), params: { game: { attendance: @game.attendance, away_id_id: @game.away_id_id, away_line_scores: @game.away_line_scores, away_points: @game.away_points, cfbd_id: @game.cfbd_id, completed: @game.completed, highlights: @game.highlights, home_id_id: @game.home_id_id, home_line_scores: @game.home_line_scores, home_points: @game.home_points, notes: @game.notes, season: @game.season, start: @game.start, venue_id_id: @game.venue_id_id } }
-    assert_redirected_to game_url(@game)
-  end
-
-  test "should destroy game" do
-    assert_difference("Game.count", -1) do
-      delete game_url(@game)
-    end
-
-    assert_redirected_to games_url
-  end
+  # test "should get index" do
+  #   get games_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get new" do
+  #   get new_game_url
+  #   assert_response :success
+  # end
+  #
+  # test "should create game" do
+  #   assert_difference("Game.count") do
+  #     post games_url, params: { game: { attendance: @game.attendance, away_id_id: @game.away_id_id, away_line_scores: @game.away_line_scores, away_points: @game.away_points, cfbd_id: @game.cfbd_id, completed: @game.completed, highlights: @game.highlights, home_id_id: @game.home_id_id, home_line_scores: @game.home_line_scores, home_points: @game.home_points, notes: @game.notes, season: @game.season, start: @game.start, venue_id_id: @game.venue_id_id } }
+  #   end
+  #
+  #   assert_redirected_to game_url(Game.last)
+  # end
+  #
+  # test "should show game" do
+  #   get game_url(@game)
+  #   assert_response :success
+  # end
+  #
+  # test "should get edit" do
+  #   get edit_game_url(@game)
+  #   assert_response :success
+  # end
+  #
+  # test "should update game" do
+  #   patch game_url(@game), params: { game: { attendance: @game.attendance, away_id_id: @game.away_id_id, away_line_scores: @game.away_line_scores, away_points: @game.away_points, cfbd_id: @game.cfbd_id, completed: @game.completed, highlights: @game.highlights, home_id_id: @game.home_id_id, home_line_scores: @game.home_line_scores, home_points: @game.home_points, notes: @game.notes, season: @game.season, start: @game.start, venue_id_id: @game.venue_id_id } }
+  #   assert_redirected_to game_url(@game)
+  # end
+  #
+  # test "should destroy game" do
+  #   assert_difference("Game.count", -1) do
+  #     delete game_url(@game)
+  #   end
+  #
+  #   assert_redirected_to games_url
+  # end
 end
--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -5,63 +5,63 @@ class GamesTest < ApplicationSystemTestCase
     @game = games(:one)
   end
 
-  test "visiting the index" do
-    visit games_url
-    assert_selector "h1", text: "Games"
-  end
-
-  test "should create game" do
-    visit games_url
-    click_on "New game"
-
-    fill_in "Attendance", with: @game.attendance
-    fill_in "Away id", with: @game.away_id_id
-    fill_in "Away line scores", with: @game.away_line_scores
-    fill_in "Away points", with: @game.away_points
-    fill_in "Cfbd", with: @game.cfbd_id
-    check "Completed" if @game.completed
-    fill_in "Highlights", with: @game.highlights
-    fill_in "Home id", with: @game.home_id_id
-    fill_in "Home line scores", with: @game.home_line_scores
-    fill_in "Home points", with: @game.home_points
-    fill_in "Notes", with: @game.notes
-    fill_in "Season", with: @game.season
-    fill_in "Start", with: @game.start
-    fill_in "Venue id", with: @game.venue_id_id
-    click_on "Create Game"
-
-    assert_text "Game was successfully created"
-    click_on "Back"
-  end
-
-  test "should update Game" do
-    visit game_url(@game)
-    click_on "Edit this game", match: :first
-
-    fill_in "Attendance", with: @game.attendance
-    fill_in "Away id", with: @game.away_id_id
-    fill_in "Away line scores", with: @game.away_line_scores
-    fill_in "Away points", with: @game.away_points
-    fill_in "Cfbd", with: @game.cfbd_id
-    check "Completed" if @game.completed
-    fill_in "Highlights", with: @game.highlights
-    fill_in "Home id", with: @game.home_id_id
-    fill_in "Home line scores", with: @game.home_line_scores
-    fill_in "Home points", with: @game.home_points
-    fill_in "Notes", with: @game.notes
-    fill_in "Season", with: @game.season
-    fill_in "Start", with: @game.start.to_s
-    fill_in "Venue id", with: @game.venue_id_id
-    click_on "Update Game"
-
-    assert_text "Game was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Game" do
-    visit game_url(@game)
-    click_on "Destroy this game", match: :first
-
-    assert_text "Game was successfully destroyed"
-  end
+  # test "visiting the index" do
+  #   visit games_url
+  #   assert_selector "h1", text: "Games"
+  # end
+  #
+  # test "should create game" do
+  #   visit games_url
+  #   click_on "New game"
+  #
+  #   fill_in "Attendance", with: @game.attendance
+  #   fill_in "Away id", with: @game.away_id_id
+  #   fill_in "Away line scores", with: @game.away_line_scores
+  #   fill_in "Away points", with: @game.away_points
+  #   fill_in "Cfbd", with: @game.cfbd_id
+  #   check "Completed" if @game.completed
+  #   fill_in "Highlights", with: @game.highlights
+  #   fill_in "Home id", with: @game.home_id_id
+  #   fill_in "Home line scores", with: @game.home_line_scores
+  #   fill_in "Home points", with: @game.home_points
+  #   fill_in "Notes", with: @game.notes
+  #   fill_in "Season", with: @game.season
+  #   fill_in "Start", with: @game.start
+  #   fill_in "Venue id", with: @game.venue_id_id
+  #   click_on "Create Game"
+  #
+  #   assert_text "Game was successfully created"
+  #   click_on "Back"
+  # end
+  #
+  # test "should update Game" do
+  #   visit game_url(@game)
+  #   click_on "Edit this game", match: :first
+  #
+  #   fill_in "Attendance", with: @game.attendance
+  #   fill_in "Away id", with: @game.away_id_id
+  #   fill_in "Away line scores", with: @game.away_line_scores
+  #   fill_in "Away points", with: @game.away_points
+  #   fill_in "Cfbd", with: @game.cfbd_id
+  #   check "Completed" if @game.completed
+  #   fill_in "Highlights", with: @game.highlights
+  #   fill_in "Home id", with: @game.home_id_id
+  #   fill_in "Home line scores", with: @game.home_line_scores
+  #   fill_in "Home points", with: @game.home_points
+  #   fill_in "Notes", with: @game.notes
+  #   fill_in "Season", with: @game.season
+  #   fill_in "Start", with: @game.start.to_s
+  #   fill_in "Venue id", with: @game.venue_id_id
+  #   click_on "Update Game"
+  #
+  #   assert_text "Game was successfully updated"
+  #   click_on "Back"
+  # end
+  #
+  # test "should destroy Game" do
+  #   visit game_url(@game)
+  #   click_on "Destroy this game", match: :first
+  #
+  #   assert_text "Game was successfully destroyed"
+  # end
 end
