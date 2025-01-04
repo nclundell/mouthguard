8969838220b2cae3b14d812fa50955e63240357c
Nathan Lundell <nclundell@pm.me>
2025-01-04 17:30:37 -0500

    disable leader tests (for now)

--- test/controllers/leaders_controller_test.rb
+++ test/controllers/leaders_controller_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 class LeadersControllerTest < ActionDispatch::IntegrationTest
-  test "should get only:" do
-    get leaders_only:_url
-    assert_response :success
-  end
-
-  test "should get index" do
-    get leaders_index_url
-    assert_response :success
-  end
+  # test "should get only:" do
+  #   get leaders_only:_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get index" do
+  #   get leaders_index_url
+  #   assert_response :success
+  # end
 end
