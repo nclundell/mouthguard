75d5c3254855baa6245a9ecbafb9b7719e6cf908
Nathan Lundell <nclundell@pm.me>
2025-01-24 17:40:47 -0500

    shorten jobs kept to one day

--- app/jobs/clean_up_job.rb
+++ app/jobs/clean_up_job.rb
@@ -2,6 +2,6 @@ class CleanUpJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    SolidQueue::Job.where("finished_at < ?", 1.week.ago).destroy_all
+    SolidQueue::Job.where("finished_at < ?", 1.day.ago).destroy_all
   end
 end
