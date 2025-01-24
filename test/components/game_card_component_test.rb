b4a19d0853249bcea037fb786ef775e798622e15
Nathan Lundell <nclundell@pm.me>
2025-01-24 13:49:30 -0500

    switch to game card component

---
+++ test/components/game_card_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class GameCardComponentTest < ViewComponent::TestCase
+  # def test_component_renders_something_useful
+  #   # assert_equal(
+  #   #   %(<span>Hello, components!</span>),
+  #   #   render_inline(GameCardComponent.new(message: "Hello, components!")).css("span").to_html
+  #   # )
+  # end
+end
