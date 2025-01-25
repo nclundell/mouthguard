d540dbfd16307dbd7b574234577dd8dcc3b25da1
Nathan Lundell <nclundell@pm.me>
2025-01-25 14:46:59 -0500

    update card component to fix weird height alignment issue

--- app/components/game_card_component.rb
+++ app/components/game_card_component.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class GameCardComponent < ViewComponent::Base
-  def initialize(game:)
+  def initialize(game:, title: true, extra_classes:)
     @game = game
+    @title = title
+    @extra_classes = extra_classes
   end
 end
