6065add49113b6695b8f74d8a0f4882b80322bbf
Nathan Lundell <nclundell@pm.me>
2025-04-09 20:49:34 -0400

    clear out game_comment test files (model dropped)

--- test/controllers/game_comments_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class GameCommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/game_comment_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class GameCommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
