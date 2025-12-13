6defc55cdc9f04f0d9ef18fc2a3ecbbca6fd6d2b
Nathan Lundell <nclundell@pm.me>
2025-12-13 11:43:21 -0500

    game card component extra_classes defaults to ""

--- app/components/game_card_component.rb
+++ app/components/game_card_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class GameCardComponent < ViewComponent::Base
-  def initialize(game:, title: true, extra_classes:)
+  def initialize(game:, title: true, extra_classes: "")
     @game = game
     @title = title
     @extra_classes = extra_classes
